315867933065f410a96188bad686150fb1cfcd39
Max Saparov <max.saparov@gmail.com>
2022-11-19 14:19:07 -0500

    parsing args and logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use clap::Parser;
+use log;
+
 mod encode;
 use crate::encode::encode_str;
 
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// list of files to parse
+    #[arg(required = true)]
+    file: Vec<String>,
+
+    /// number of threads to create
+    #[arg(short, long, default_value_t = 1)]
+    jobs: u8,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Args::parse();
+
+    log::debug!("files: {:?} - jobs: {:?}", args.file, args.jobs);
     encode_str("aaa");
 }
